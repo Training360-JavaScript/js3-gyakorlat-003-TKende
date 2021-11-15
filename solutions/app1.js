995f8e3df6b08436421903e7030272964e86c0a4
TKende <kendetompos@gmail.com>
2021-11-15 16:27:47 +0100

    ?

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,1 +1,8 @@
+function setCookie(value) {
+    const now = new Date();
+    now.setTime(now.getTime() + (1000 * 60 * 15));
+    document.cookie = `token=${value};expires=${now.toUTCString};path=/`
 
+}
+
+export default setCookie;
